bc0b29fee2b1adfb02d9f67b9e3181b429e336db
Emil Świniarski <emil.swiniarski7@gmail.com>
2018-12-23 16:36:27 +0100

    Implemented csv class Fixes #2

--- src/csv.php
+++ src/csv.php
@@ -7,11 +7,62 @@ namespace ES\Utils;
  * This class provides simaple operations on csv files
  */
 class CSV {
-    public static function toArray($csvPath) {
 
+    /**
+     * Reads csv file to array
+     *
+     * @param string $csvPath
+     * @return array
+     */
+    public static function toArray($csvPath) {
+        $array = [];
+        $headers = [];
+        $i = 0;
+        $handle = fopen($csvPath, 'r');
+
+        if ($handle) {
+            while (($row = fgetcsv($handle)) !== false) {        
+                if (empty($headers)) {
+                    $headers = $row;filePath
+                    continue;
+                }
+
+                foreach ($row as $k => $value) {
+                    $array[$i][$headers[$k]] = $value;
+                }
+
+                $i++;
+            }
+            
+            fclose($handle);
+        }
+
+        return $array;
     }
 
-    public static function toCsv($csvRows) {
+    /**
+     * Saves array to csv file
+     *
+     * @param array $csvRows
+     * @param string  $filePath
+     * @param bolean  $firstRowAsHeader
+     * @return array
+     */
+    public static function toCsv($csvRows, $filePath, $firstRowAsHeader = true) {
+        $fp = fopen($filePath, 'a');
+        $headers = [];
+        $csvTmpRow = [];
 
+        if (true === $firstRowAsHeader) {
+            if (isset($csvRows[0]) && is_array($csvRows[0])) {
+                $headers = array_keys($csvRows[0]);
+            }
+
+            fputcsv($fp, $headers);
+        }
+
+        foreach ($csvRows as $k => $row) {
+            fputcsv($fp, $row);
+        }
     }
 }
